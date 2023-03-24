b8e8bfc6206d0cc41b5018b4dc72a6a632fb3ede
PaNTsuRoRi <zestosaniota@yandex.ru>
2023-03-24 23:01:08 +0300

    add lab5

---
+++ МиАЗИ/lab5/lab5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+#include "windows.h"
+
+using namespace std;
+
+int p = 31;
+int q = 3;
+int a = 5;
+
+int getSum(char sym) {
+
+}
+
+int getHash(string str) {
+    
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    srand(time(0));
+    int x = rand() % 3 + 1;
+    int y = pow(a, x);
+    y = y % p;
+}
